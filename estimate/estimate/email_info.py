34aaca90089d583a74f5a0c911ae00cf62dd263b
tusharsri1998 <33591937+tusharsri1998@users.noreply.github.com>
2019-09-04 12:07:18 +0530

    Update email_info.py

--- estimate/estimate/email_info.py
+++ estimate/estimate/email_info.py
@@ -1,5 +1,5 @@
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'ts94553@gmail.com'
-EMAIL_HOST_PASSWORD = 'killerbong@1'
+EMAIL_HOST_USER = 'your email id'
+EMAIL_HOST_PASSWORD = 'your password'
 EMAIL_PORT = 587
